56e875498fbf388af4e2a25e68ae81f7bab36564
gollapalliramya <105625326+gollapalliramya@users.noreply.github.com>
2022-05-31 15:25:51 +0530

    initial commit

---
+++ ThreadPriority.cs
@@ -0,0 +1,30 @@
+using System;  
+using System.Threading;  
+public class MyThread  
+{  
+public void Thread1()  
+{  
+Thread t = Thread.CurrentThread;  
+Console.WriteLine(t.Name+" is running");  
+}  
+}  
+public class ThreadExample  
+{  
+public static void Main()  
+{  
+MyThread mt = new MyThread();  
+Thread t1 = new Thread(new ThreadStart(mt.Thread1));  
+Thread t2 = new Thread(new ThreadStart(mt.Thread1));  
+Thread t3 = new Thread(new ThreadStart(mt.Thread1));  
+t1.Name = "Player1";  
+t2.Name = "Player2";  
+t3.Name = "Player3";  
+t3.Priority = ThreadPriority.Highest;  
+t2.Priority = ThreadPriority.Normal;  
+t1.Priority = ThreadPriority.Lowest;  
+
+t1.Start();  
+t2.Start();  
+t3.Start();  
+}  
+}  
